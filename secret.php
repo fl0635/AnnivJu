9c90fe593fdc9c3bdb726269d993116c4e14f0a6
Gautier Florian <floriange97@hotmail.fr>
2018-11-11 22:11:49 +0100

    fin

--- secret.php
+++ secret.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>Codes d'accès au serveur central de la NASA</title>
+    <title>Video anniv Juliette</title>
 </head>
 <body>
 
@@ -11,13 +11,9 @@ if (isset($_POST['mot_de_passe']) AND $_POST['mot_de_passe'] ==  "annivjuliette"
 {
     // On affiche les codes
     ?>
-    <h1>Voici les codes d'accès :</h1>
-    <p><strong>CRD5-GTFT-CK65-JOPM-V29N-24G1-HH28-LLFV</strong></p>
+    <h1>Voici la video à apprendre :</h1>
+    <video controls src="video-1539024472.mp4"></video>
 
-    <p>
-        Cette page est réservée au personnel de la NASA. N'oubliez pas de la visiter régulièrement car les codes d'accès sont changés toutes les semaines.<br />
-        La NASA vous remercie de votre visite.
-    </p>
     <?php
 }
 else // Sinon, on affiche un message d'erreur
